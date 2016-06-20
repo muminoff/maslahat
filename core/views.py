a36f39c7e1a33f97360c39e13c535e1bd00d068e
Sardor Muminov <sardor@insilicogen.com>
2016-06-20 14:20:32 +0900

    Post model

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
 
 # Create your views here.
 def index(request):
--- maslahat/urls.py
+++ maslahat/urls.py
@@ -11,6 +11,5 @@ import core.views
 
 urlpatterns = [
     url(r'^$', core.views.index, name='index'),
-    url(r'^db', core.views.db, name='db'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
