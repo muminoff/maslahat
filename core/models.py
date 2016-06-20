9b0dcd8cbdbe74843169d4a6c44a0f8b2e50d9de
Sardor Muminov <sardor@insilicogen.com>
2016-06-20 12:52:54 +0900

    Post model create

--- core/models.py
+++ core/models.py
@@ -3,3 +3,20 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+
+class Post(models.Model):
+    id = models.CharField(primary_key=True, max_length=256)
+    text = models.TextField()
+    author = models.TextField()
+    link = models.TextField()
+    type = models.TextField()
+    published = models.DateTimeField()
+    reactions = models.IntegerField()
+    comments = models.IntegerField()
+    shares = models.IntegerField()
+    likes = models.IntegerField()
+    loves = models.IntegerField()
+    hahas = models.IntegerField()
+    sads = models.IntegerField()
+    angrys = models.IntegerField()
