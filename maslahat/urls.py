0a06130a82fc8c7c8b1642d77cb183ffe733b949
Sardor Muminov <sardor@insilicogen.com>
2016-06-20 11:10:58 +0900

    App name change

--- maslahat/urls.py
+++ maslahat/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import core.views
 
 # Examples:
 # url(r'^$', 'maslahat.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', core.views.index, name='index'),
+    url(r'^db', core.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
