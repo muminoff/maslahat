ef319f0ddd44196f140d15429dd5b4ffa39ef721
Sardor Muminov <sardor@insilicogen.com>
2016-06-20 14:19:29 +0900

    No db route

--- core/models.py
+++ core/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
-
 
 class Post(models.Model):
     id = models.CharField(primary_key=True, max_length=256)
