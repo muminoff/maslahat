49f34b5d93bb55efe60440f244e89993e1bd6e60
Sardor Muminov <sardor@insilicogen.com>
2016-07-10 20:41:14 +0900

    os

--- maslahat/settings.py
+++ maslahat/settings.py
@@ -17,6 +17,7 @@ from urllib.parse import urlparse
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DEBUG = True
 
 
 # Quick-start development settings - unsuitable for production
