1f378646aeb16fd5fbc11d92f20204e403f01a1f
Sardor Muminov <sardor@insilicogen.com>
2016-08-03 10:50:06 +0900

    HTML minify

--- maslahat/settings.py
+++ maslahat/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
 )
 
 ROOT_URLCONF = 'maslahat.urls'
