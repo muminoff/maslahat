8ad92b3a2a97caee22061c933d03cb6195f26fb2
Sardor Muminov <sardor@insilicogen.com>
2016-12-22 13:27:49 +0900

    No debug

--- maslahat/settings.py
+++ maslahat/settings.py
@@ -13,8 +13,6 @@ import os
 import dj_database_url
 from urllib.parse import urlparse
 
-DEBUG = True
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
